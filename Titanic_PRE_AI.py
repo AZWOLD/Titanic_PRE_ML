606e55d990bb5276d0b22addcb5a5e423e5ea7c3
AZWOLD <faycalkdj0@gmail.com>
2025-05-19 14:01:29 +0100

    Finished Cleaning all of the DataFrame(Data CSV file) || DataFrame now is ready to be analyzed

--- Titanic_PRE_AI.py
+++ Titanic_PRE_AI.py
@@ -7,4 +7,7 @@ Data_df = pd.read_csv("train.csv")
 #Basically sorting/cleaniing the DataFrame for usability:
 Data_df = Data_df.set_index("PassengerId").drop(columns=["Name"])
 Data_df["Sex"] = Data_df["Sex"].map({"male": 1,"female":0})
+Data_df["Embarked"] = Data_df["Embarked"].map({"S": 0,"C":1,"Q":2})
 Data_df["Survived"] = Data_df.pop("Survived")
+Data_df["Age"].fillna(int(Data_df["Age"].mean()),inplace=True)
+Data_df["Cabin"] = Data_df["Cabin"].notna().astype(int)
