8d5f82c17c1e58ec5b8bf0ac5bd8553cef3a3309
AZWOLD <faycalkdj0@gmail.com>
2025-05-19 15:17:04 +0100

    Loaded Test Data,Cleaned/Sorted/Reashaped it and fed it to the model

--- Titanic_PRE_AI.py
+++ Titanic_PRE_AI.py
@@ -15,11 +15,23 @@ TR_Data_df["Survived"] = TR_Data_df.pop("Survived")
 TR_Data_df["Age"].fillna(int(TR_Data_df["Age"].mean()),inplace=True)
 TR_Data_df["Cabin"] = TR_Data_df["Cabin"].notna().astype(int)
 
-#Reshaping data for Inputing:
+#Cleaning/Sorting Test DataFrame:
+TS_Data_df = TS_Data_df.set_index("PassengerId").drop(columns=["Name","Ticket"])
+TS_Data_df["Sex"] = TS_Data_df["Sex"].map({"male":0,"female":1})
+TS_Data_df["Embarked"] = TS_Data_df["Embarked"].map({"S":0,"C":1,"Q":2})
+TS_Data_df["Age"].fillna(int(TS_Data_df["Age"].mean()),inplace=True)
+TS_Data_df["Fare"].fillna(int(TS_Data_df["Fare"].mean()),inplace=True)
+TS_Data_df["Cabin"] = TS_Data_df["Cabin"].notna().astype(int)
+
+#Reshaping data for Fitting and predicting:
 X_Data = np.array(TR_Data_df[["Pclass","Sex","Age","SibSp","Parch","Fare","Cabin"]]).reshape(891,7)
 Y_Data = np.array(TR_Data_df["Survived"])
+X_TS_Data = np.array(TS_Data_df[["Pclass","Sex","Age","SibSp","Parch","Fare","Cabin"]]).reshape(418,7)
 
 #Loading the model from Sklearn and fitting it with the data:
 LR_model = linear_model.LogisticRegression()
 LR_model.fit(X_Data,Y_Data)
 
+#Predicting:
+PRE_Y_Data = LR_model.predict(X_TS_Data)
+TS_Data_df["Survived"] = PRE_Y_Data
